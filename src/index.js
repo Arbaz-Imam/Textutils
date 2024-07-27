6eed68f61dc8ffa0d96f134dd48909b0981c3583
Arbaz <arbazimam1250@gmail.com>
2024-07-27 16:41:43 +0530

    remove build folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 
 
-const myFirstElement = <h1>Hello React!</h1>
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App></App>);
